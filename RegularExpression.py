7018568d66e44ee75410e4dabdd331f82e592efe
MEETPATEL96111 <62256103+MEETPATEL96111@users.noreply.github.com>
2020-10-01 00:28:16 +0530

    Update RegularExpression.py

--- RegularExpression.py
+++ RegularExpression.py
@@ -1,7 +1,7 @@
 import  re
 phrase="my name is Durgaprasad and my email id durgaprasad8883@gmail.com, jyos emaill id gsjyothirani@gmail.com"
 #email Scrapping
-pattern=re.compile("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+")
+pattern=re.compile("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+") //change the Regex equation it is best ever equation for email Scrapping
 #result=pattern.search(phrase) geeting single email
 result=pattern.findall(phrase)
 print(result)
