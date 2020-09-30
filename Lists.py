ffff89313c1ad096ea0477710b27db56f1fdea89
MEETPATEL96111 <62256103+MEETPATEL96111@users.noreply.github.com>
2020-10-01 00:17:27 +0530

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,6 +1,6 @@
 names=["Joe","John","James"]
-names.insert(0,"jennifer")
-names.append("justin")
+names.insert(0,"jennifer") // insert element at 0th position in list
+names.append("justin") // add new element in list
 
 numbers=[1107,1935,198]
 numbers.sort()
