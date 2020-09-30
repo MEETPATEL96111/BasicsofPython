df35d19d12051cbfffc1c41db73ab3837e26e248
MEETPATEL96111 <62256103+MEETPATEL96111@users.noreply.github.com>
2020-10-01 00:25:13 +0530

    Update RegularExpression.py

--- RegularExpression.py
+++ RegularExpression.py
@@ -1,8 +1,7 @@
 import  re
 phrase="my name is Durgaprasad and my email id durgaprasad8883@gmail.com, jyos emaill id gsjyothirani@gmail.com"
 #email Scrapping
-pattern=re.compile("[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+pattern=re.compile("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+")
 #result=pattern.search(phrase) geeting single email
 result=pattern.findall(phrase)
 print(result)
-
